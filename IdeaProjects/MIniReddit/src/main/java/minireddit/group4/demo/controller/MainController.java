f13c5ecff7789fe495c2a6d43063b26f71477270
harambe143 <lava31@mail.com>
2017-08-24 17:52:58 -0400

    everything works

--- IdeaProjects/MIniReddit/src/main/java/minireddit/group4/demo/controller/MainController.java
+++ IdeaProjects/MIniReddit/src/main/java/minireddit/group4/demo/controller/MainController.java
@@ -6,10 +6,13 @@ import minireddit.group4.demo.repository.RedditRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Date;
 
 @Controller
@@ -34,9 +37,13 @@ public class MainController
     }
 
     @PostMapping("/entryform")
-    public String postForm(@ModelAttribute("newReddit") Reddit otherreddit, Model model)
+    public String postForm(@Valid@ModelAttribute("newReddit") Reddit otherreddit, BindingResult bindingResult, Model model)
     {
 
+        if(bindingResult.hasErrors())
+        {
+            return "entryform";
+        }
         Date date=new Date();
        model.addAttribute("new", otherreddit);
         otherreddit.setDate(date);
@@ -51,5 +58,26 @@ public class MainController
         Iterable<Reddit> rd =  redditRepository.findAll();
         model.addAttribute("redobj", rd);
         return "showall";
+    }
+    @RequestMapping("/login")
+    public String login()
+    {
+        return "login";
+    }
+    @GetMapping("/searchname")
+    public String searchName(Model model)
+    {
+
+        model.addAttribute("searchReddit",new Reddit());
+        return "reddiesearch";
+    }
+    @PostMapping("/searchname")
+    public String searchDishMethod(@ModelAttribute("searchReddit") Reddit reddit, Model model)
+    {
+
+        Iterable<Reddit>  listLink= redditRepository.findAllByname(reddit.getName());
+        model.addAttribute("llink",listLink);
+
+        return "reddieresult";
     }
 }
--- IdeaProjects/MIniReddit/src/main/java/minireddit/group4/demo/model/Reddit.java
+++ IdeaProjects/MIniReddit/src/main/java/minireddit/group4/demo/model/Reddit.java
@@ -1,6 +1,12 @@
 package minireddit.group4.demo.model;
 
 
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,9 +20,13 @@ public class Reddit {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    @NotEmpty
    private String name;
+    @NotEmpty
     private String title;
+
     private String hyperlink;
+    @DateTimeFormat(pattern="MMM dd,yyyy")
     private Date date;
 
 
@@ -61,6 +71,7 @@ public class Reddit {
     public void setId(long id) {
         this.id = id;
     }
+
 
 
 }
